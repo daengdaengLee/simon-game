af081bcb4241d7b1794644617821485092f4824d
KunhoLee <gunho1020@gmail.com>
2018-02-27 19:44:33 +0900

    ENH: 사용자가 버튼 클릭하면 player state 변경
    _playerInput method: buttonId params 에 따라 player state 변경

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ class App extends Component {
     };
     this._start = this._start.bind(this);
     this._clearActiveButton = this._clearActiveButton.bind(this);
+    this._playerInput = this._playerInput.bind(this);
   }
 
   _start() {
@@ -25,17 +26,33 @@ class App extends Component {
     this.setState({ activeButton: null });
   }
 
+  _playerInput(buttonId) {
+    this.setState({ player: buttonId });
+  }
+
   render() {
-    const { _start } = this;
+    const { _start, _playerInput } = this;
     const { activeButton } = this.state;
     return (
       <div>
         <span className="My__Display"></span>
         <button className="My__StartButton" onClick={_start}>Start</button>
-        <button className={activeButton === 1 ? 'My__Button__Active' : null}>1</button>
-        <button className={activeButton === 2 ? 'My__Button__Active' : null}>2</button>
-        <button className={activeButton === 3 ? 'My__Button__Active' : null}>3</button>
-        <button className={activeButton === 4 ? 'My__Button__Active' : null}>4</button>
+        <button
+          className={activeButton === 1 ? 'My__Button__Active' : null}
+          onClick={() => _playerInput(1)}
+        >1</button>
+        <button
+          className={activeButton === 2 ? 'My__Button__Active' : null}
+          onClick={() => _playerInput(2)}
+        >2</button>
+        <button
+          className={activeButton === 3 ? 'My__Button__Active' : null}
+          onClick={() => _playerInput(3)}
+        >3</button>
+        <button
+          className={activeButton === 4 ? 'My__Button__Active' : null}
+          onClick={() => _playerInput(4)}
+        >4</button>
       </div>
     );
   }
