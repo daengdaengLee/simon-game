8ab81c503439b6ff3cd184821f7a8906aa1fdff8
KunhoLee <gunho1020@gmail.com>
2018-02-27 19:17:39 +0900

    ENH: _start 메소드 작성
    스타트 버튼 클릭하면 1, 2, 3, 4 중 랜덤한 숫자를 game state 에 할당

--- src/App.js
+++ src/App.js
@@ -9,13 +9,20 @@ class App extends Component {
       game: null,
       player: null,
     };
+    this._start = this._start.bind(this);
+  }
+
+  _start() {
+    const game = Math.round(Math.random() * 3) + 1;
+    this.setState({ game });
   }
 
   render() {
+    const { _start } = this;
     return (
       <div>
         <span className="My__Display"></span>
-        <button className="My__StartButton">Start</button>
+        <button className="My__StartButton" onClick={_start}>Start</button>
         <button>1</button>
         <button>2</button>
         <button>3</button>
