8b8b0d79580e8c8f9b14e16ab2fa6252f20ded1c
KunhoLee <gunho1020@gmail.com>
2018-02-27 23:53:44 +0900

    ENH: 성공했을 경우 다음 스테이지로 넘어가도록 기능 추가

--- src/App.js
+++ src/App.js
@@ -12,15 +12,20 @@ class App extends Component {
       activeButton: null,
     };
     this._start = this._start.bind(this);
+    this._activateButton = this._activateButton.bind(this);
     this._clearActiveButton = this._clearActiveButton.bind(this);
+    this._showGame = this._showGame.bind(this);
     this._playerInput = this._playerInput.bind(this);
+    this._successFail = this._successFail.bind(this);
+    this._resetPlayer = this._resetPlayer.bind(this);
+    this._nextStage = this._nextStage.bind(this);
   }
 
   _start() {
-    const { _clearActiveButton, _makeGame } = this;
+    const { _makeGame, _showGame } = this;
     const game = _makeGame(1);
-    this.setState({ game, activeButton: game[0] });
-    setTimeout(_clearActiveButton, 1500);
+    setTimeout(() => _showGame(game), 500);
+    this.setState({ game, count: 1 });
   }
 
   _makeGame(count) {
@@ -31,23 +36,71 @@ class App extends Component {
     return game;
   }
 
+  _showGame(game) {
+    const { _activateButton, _clearActiveButton } = this;
+    let delay = 1000;
+    for(let i = 0; i < game.length; i += 1) {
+      setTimeout(() => _activateButton(game[i]), delay);
+      delay += 1000;
+      if(i === game.length - 1) setTimeout(_clearActiveButton, delay + 1000);
+    }
+  }
+
+  _activateButton(buttonId) {
+    this.setState({ activeButton: buttonId });
+  }
+
   _clearActiveButton() {
     this.setState({ activeButton: null });
   }
 
   _playerInput(buttonId) {
-    this.setState({ player: [buttonId] });
+    this.setState(prevState => {
+      return { player: [...prevState.player, buttonId] };
+    });
+  }
+
+  _successFail(game, player) {
+    const { _nextStage, _resetPlayer } = this;
+    const len = player.length;
+    if(game[len - 1] !== player[len - 1]) {
+      setTimeout(_resetPlayer, 1000);
+      return 'FAIL';
+    }
+    if(game.length === len) {
+      setTimeout(_nextStage, 1000);
+      return 'SUCCESS';
+    }
+  }
+
+  _nextStage() {
+    const { _makeGame, _showGame } = this;
+    const { count } = this.state;
+    const game = _makeGame(count + 1);
+    setTimeout(() => _showGame(game), 500);
+    this.setState({
+      count: count + 1,
+      game,
+      player: [],
+    });
+  }
+
+  _resetPlayer() {
+    this.setState({
+      player: [],
+    });
   }
 
   render() {
-    const { _start, _playerInput } = this;
+    const { _start, _playerInput, _successFail } = this;
     const { activeButton, game, player } = this.state;
     return (
       <div>
         <span className="My__Display">
           {
-            game.length === 0 || player.length === 0 ? '' :
-              game[0] === player[0] ? 'SUCCESS' : 'FAIL'
+            game.length === 0 || player.length === 0
+              ? ''
+              : _successFail(game, player)
           }
         </span>
         <button className="My__StartButton" onClick={_start}>Start</button>
