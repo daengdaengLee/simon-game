bbef8c3875a93a56ef9f20d9bc1c3e76b3778ceb
KunhoLee <gunho1020@gmail.com>
2018-02-27 19:40:57 +0900

    ENH: 게임 버튼 배경색 1.5초 뒤에 원래대로 되돌림
    activeButton state: 배경색을 표시할 버튼 번호
    _clearActiveButton method: activeButton state 를 null 로 바꾸는 method

--- src/App.js
+++ src/App.js
@@ -8,26 +8,34 @@ class App extends Component {
     this.state = {
       game: null,
       player: null,
+      activeButton: null,
     };
     this._start = this._start.bind(this);
+    this._clearActiveButton = this._clearActiveButton.bind(this);
   }
 
   _start() {
+    const { _clearActiveButton } = this;
     const game = Math.round(Math.random() * 3) + 1;
-    this.setState({ game });
+    this.setState({ game, activeButton: game });
+    setTimeout(_clearActiveButton, 1500);
+  }
+
+  _clearActiveButton() {
+    this.setState({ activeButton: null });
   }
 
   render() {
     const { _start } = this;
-    const { game } = this.state;
+    const { activeButton } = this.state;
     return (
       <div>
         <span className="My__Display"></span>
         <button className="My__StartButton" onClick={_start}>Start</button>
-        <button className={game === 1 ? 'My__Button__Active' : null}>1</button>
-        <button className={game === 2 ? 'My__Button__Active' : null}>2</button>
-        <button className={game === 3 ? 'My__Button__Active' : null}>3</button>
-        <button className={game === 4 ? 'My__Button__Active' : null}>4</button>
+        <button className={activeButton === 1 ? 'My__Button__Active' : null}>1</button>
+        <button className={activeButton === 2 ? 'My__Button__Active' : null}>2</button>
+        <button className={activeButton === 3 ? 'My__Button__Active' : null}>3</button>
+        <button className={activeButton === 4 ? 'My__Button__Active' : null}>4</button>
       </div>
     );
   }
