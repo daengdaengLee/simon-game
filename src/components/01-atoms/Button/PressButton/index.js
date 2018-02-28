f9e0c90c0e5412007dd5f0585771b76fe0f89497
KunhoLee <gunho1020@gmail.com>
2018-02-28 11:18:28 +0900

    ENH: PressButton 배경색 결정하는 color prop 추가
    - Red, Yellow 중 택 1

--- src/components/01-atoms/Button/PressButton/index.js
+++ src/components/01-atoms/Button/PressButton/index.js
@@ -3,12 +3,13 @@ import PropTypes from 'prop-types';
 
 import './style.css';
 
-const PressButton = ({ onClick }) => (
-  <button className="My__PressButton" onClick={onClick}></button>
+const PressButton = ({ onClick, color }) => (
+  <button className={`My__PressButton My__BackgroundColor__${color}`} onClick={onClick}></button>
 );
 
 PressButton.propTypes = {
   onClick: PropTypes.func.isRequired,
+  color: PropTypes.oneOf(['Red', 'Yellow']).isRequired,
 };
 
 export default PressButton;
