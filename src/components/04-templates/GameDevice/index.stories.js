fa19dcb685be41491d9acc7da4c987c5bc7349f5
KunhoLee <gunho1020@gmail.com>
2018-03-01 19:26:21 +0900

    TST: GameDevice story 에 gameHeading prop 추가

--- src/components/04-templates/GameDevice/index.stories.js
+++ src/components/04-templates/GameDevice/index.stories.js
@@ -3,9 +3,11 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 
 import GameDevice from './index';
+import Heading from '../../01-atoms/Heading';
 
 storiesOf('Templates', module)
 .add('GameDevice', () => <GameDevice
+  gameHeading={<Heading headingNo={1}>heading</Heading>}
   gameConsole={<div style={{width: '10rem', height: '8rem', backgroundColor: 'red'}}></div>}
   gameController={<div style={{width: '10rem', height: '12rem', backgroundColor: 'yellow'}}></div>}
 />);
