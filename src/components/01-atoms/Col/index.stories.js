dd88a4f758bd68037d6a999b3b418e05aa3e6595
KunhoLee <gunho1020@gmail.com>
2018-03-01 16:43:36 +0900

    TST: Col story borderRadius prop 추가

--- src/components/01-atoms/Col/index.stories.js
+++ src/components/01-atoms/Col/index.stories.js
@@ -5,4 +5,4 @@ import { storiesOf } from '@storybook/react';
 import Col from './index';
 
 storiesOf('Atoms', module)
-.add('Col center', () => <Col justifyContent="SpaceBetween" alignItems="Center" width="5rem" height="10rem" backgroundColor="red" border="2px solid black"><span>test1</span><button>test2</button><input type="text" /></Col>);
+.add('Col', () => <Col justifyContent="SpaceBetween" alignItems="Center" width="5rem" height="10rem" backgroundColor="red" border="2px solid black" borderRadius="0.4rem"><span>test1</span><button>test2</button><input type="text" /></Col>);
