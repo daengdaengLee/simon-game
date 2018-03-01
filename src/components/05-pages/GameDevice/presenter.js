1f023156402f80c7e6f99e638b50f64c3c2ba668
KunhoLee <gunho1020@gmail.com>
2018-03-01 20:00:48 +0900

    ENH: pages/GameDevice presenter 작성

---
+++ src/components/05-pages/GameDevice/presenter.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import GameDeviceTemplate from '../../04-templates/GameDevice';
+import Heading from '../../01-atoms/Heading';
+import GameConsole from '../../03-organisms/GameConsole';
+import GameBoard from '../../02-molecules/GameBoard';
+
+const GameDevice = ({ start, display, playerInput, activeButton }) => <GameDeviceTemplate
+  gameHeading={<Heading headingNo={1}>Simon</Heading>}
+  gameConsole={<GameConsole start={start} display={display} />}
+  gameController={<GameBoard playerInput={playerInput} activeButton={activeButton} />}
+/>;
+
+GameDevice.propTypes = {
+  start: PropTypes.func.isRequired,
+  display: PropTypes.node,
+  playerInput: PropTypes.func.isRequired,
+  activeButton: PropTypes.oneOf([1, 2, 3, 4]),
+};
+
+export default GameDevice;
