7c5a80a585261c11da881120a2eafc561eb140c0
KunhoLee <gunho1020@gmail.com>
2018-03-01 13:27:30 +0900

    ENH: GameButton 컴포넌트 onClick prop 추가

--- src/components/01-atoms/GameButton/index.js
+++ src/components/01-atoms/GameButton/index.js
@@ -3,12 +3,14 @@ import PropType from 'prop-types';
 
 import './style.css';
 
-const GameButton = ({ color }) => <button
+const GameButton = ({ color, onClick }) => <button
   className={`My__GameButton My__Background__${color}`}
+  onClick={onClick}
 ></button>;
 
 GameButton.propTypes = {
   color: PropType.oneOf(['Red', 'Yellow', 'Green', 'Blue']).isRequired,
+  onClick: PropTypes.func.isRequired,
 };
 
 export default GameButton;
