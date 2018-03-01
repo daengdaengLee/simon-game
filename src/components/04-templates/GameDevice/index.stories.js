17e9f3622aa04c5993778011ac76a8e9309bf3b9
KunhoLee <gunho1020@gmail.com>
2018-03-01 19:44:47 +0900

    TST: GameDevice 스토리에 실제 컴포넌트 테스트
    - GameConsole 컴포넌트 사용
    - GameBoard 컴포넌트 사용
    - Heading 문구 Simon 으로 변경

--- src/components/04-templates/GameDevice/index.stories.js
+++ src/components/04-templates/GameDevice/index.stories.js
@@ -4,10 +4,12 @@ import { storiesOf } from '@storybook/react';
 
 import GameDevice from './index';
 import Heading from '../../01-atoms/Heading';
+import GameConsole from '../../03-organisms/GameConsole';
+import GameBoard from '../../02-molecules/GameBoard';
 
 storiesOf('Templates', module)
 .add('GameDevice', () => <GameDevice
-  gameHeading={<Heading headingNo={1}>heading</Heading>}
-  gameConsole={<div style={{width: '10rem', height: '8rem', backgroundColor: 'red'}}></div>}
-  gameController={<div style={{width: '10rem', height: '12rem', backgroundColor: 'yellow'}}></div>}
+  gameHeading={<Heading headingNo={1}>Simon</Heading>}
+  gameConsole={<GameConsole start={() => console.log('start')} display="Success" />}
+  gameController={<GameBoard playerInput={() => console.log('player input')} />}
 />);
