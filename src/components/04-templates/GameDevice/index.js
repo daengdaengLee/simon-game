14ef56733fe5b4aabf55aaba70f1c504b1a0551c
KunhoLee <gunho1020@gmail.com>
2018-03-01 19:26:37 +0900

    ENH: GameDevice 에 gameHeading prop 추가

--- src/components/04-templates/GameDevice/index.js
+++ src/components/04-templates/GameDevice/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 import Col from '../../01-atoms/Col';
 
-const GameDevice = ({ gameConsole, gameController }) => (
+const GameDevice = ({ gameConsole, gameController, gameHeading }) => (
   <Col>
+    {gameHeading}
     {gameConsole}
     {gameController}
   </Col>
