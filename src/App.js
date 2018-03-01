4fdc857731e49e144563c9bf1aaaa25938a64d56
KunhoLee <gunho1020@gmail.com>
2018-03-01 16:50:52 +0900

    TST: GameBoard 컴포넌트 App 에서 동작 확인

--- src/App.js
+++ src/App.js
@@ -2,9 +2,8 @@ import React, { Component } from 'react';
 
 import PressButton from './components/01-atoms/PressButton';
 import Display from './components/01-atoms/Display';
-import Row from './components/01-atoms/Row';
 import Col from './components/01-atoms/Col';
-import GameButton from './components/01-atoms/GameButton';
+import GameBoard from './components/02-molecules/GameBoard';
 
 import './App.css';
 
@@ -117,32 +116,7 @@ class App extends Component {
           }
         </Display>
         <PressButton onClick={_start} color="Red" />
-        <Col>
-          <Row>
-            <GameButton
-              active={activeButton === 1}
-              color="Red"
-              onClick={() => _playerInput(1)}
-            />
-            <GameButton
-              active={activeButton === 2}
-              color="Yellow"
-              onClick={() => _playerInput(2)}
-            />
-          </Row>
-          <Row>
-            <GameButton
-              active={activeButton === 3}
-              color="Blue"
-              onClick={() => _playerInput(3)}
-            />
-            <GameButton
-              active={activeButton === 4}
-              color="Green"
-              onClick={() => _playerInput(4)}
-            />
-          </Row>
-        </Col>
+        <GameBoard playerInput={_playerInput} activeButton={activeButton} />
       </Col>
     );
   }
