4db73cd026898aaea8c12b31d1af042d2e7cfbd6
KunhoLee <gunho1020@gmail.com>
2018-03-01 11:39:50 +0900

    TST: Display 컴포넌트 story 작성
    children prop 으로 넘긴 내용 표시하는 기능

---
+++ src/components/01-atoms/Display/index.stories.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+
+import Display from './index';
+
+storiesOf('Display', module)
+.add('display', () => <Display>START</Display>);
