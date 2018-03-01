6bc244a75a705c1ae9042236baaca9eeb479ef31
KunhoLee <gunho1020@gmail.com>
2018-03-01 16:34:33 +0900

    TST: Col story width, height, backgroundColor, border prop 추가

--- src/components/01-atoms/Col/index.stories.js
+++ src/components/01-atoms/Col/index.stories.js
@@ -5,4 +5,4 @@ import { storiesOf } from '@storybook/react';
 import Col from './index';
 
 storiesOf('Atoms', module)
-.add('Col center', () => <Col justifyContent="Center" alignItems="Center"><span>test1</span><button>test2</button><input type="text" /></Col>);
+.add('Col center', () => <Col justifyContent="Center" alignItems="Center" width="5rem" height="10rem" backgroundColor="red" border="2px solid black"><span>test1</span><button>test2</button><input type="text" /></Col>);
