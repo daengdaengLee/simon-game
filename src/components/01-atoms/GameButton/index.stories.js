e1cff749da6033544df0528d8873326d968dbefd
KunhoLee <gunho1020@gmail.com>
2018-03-01 13:41:39 +0900

    TST: GameButton active prop 추가
    active prop 이 true 이면 active 클래스 추가

--- src/components/01-atoms/GameButton/index.stories.js
+++ src/components/01-atoms/GameButton/index.stories.js
@@ -5,7 +5,7 @@ import { storiesOf } from '@storybook/react';
 import GameButton from './index';
 
 storiesOf('GameButton', module)
-.add('no 1', () => <GameButton color="Red" onClick={() => console.log('clicked')} />)
-.add('no 2', () => <GameButton color="Yellow" onClick={() => console.log('clicked')} />)
-.add('no 3', () => <GameButton color="Green" onClick={() => console.log('clicked')} />)
-.add('no 4', () => <GameButton color="Blue" onClick={() => console.log('clicked')} />);
+.add('no 1', () => <GameButton color="Red" onClick={() => console.log('clicked')} active={true} />)
+.add('no 2', () => <GameButton color="Yellow" onClick={() => console.log('clicked')} active={false} />)
+.add('no 3', () => <GameButton color="Green" onClick={() => console.log('clicked')} active={true} />)
+.add('no 4', () => <GameButton color="Blue" onClick={() => console.log('clicked')} active={false} />);
